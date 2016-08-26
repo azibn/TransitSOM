524c154c68fb9482ff25363771595abfdac4f0d2
David Armstrong <DJArmstrong@users.noreply.github.com>
2016-08-26 10:35:17 +0100

    Added more function description

--- TransitSOM_Release/TransitSOM_release.py
+++ TransitSOM_Release/TransitSOM_release.py
@@ -1,20 +1,31 @@
+#convenience functions for TransitSOM release
 
 import somtools
+#reload(somtools)
 import utils
-reload(utils)
+#reload(utils)
 import selfsom
 import os
 import numpy as np
 
-#convenience functions for TransitSOM release
 
- #prepare lightcurve(s)
- #lightcurves in format time, flux, error
- #lightcurves which do not exist or which have no points in 3 duration window are skipped
- #empty bins will be interpolated
+
 def PrepareLightcurves(filelist,periods,t0s,tdurs,nbins=50):
 
-    
+    #    Takes a list of lightcurve files and produces a SOM array suitable for classification or training.
+    #
+    #        Args:
+    #            filelist: List of lightcurve files. Files should be in format time, flux, error. 
+    #            periods: Orbital periods, one per input file.
+    #            t0s: Epochs, one per input file
+    #            tdurs: Transit durations, one per input file
+    #            nbins: Number of bins to make across 3 transit duration window centred on transit. Empty bins will be interpolated linearly.
+    #          
+    #        Returns:
+    #            SOMarray: Array of binned normalised lightcurves
+    #            SOMarray_errors: Array of binned lightcurve errors
+    #            som_ids: The index in filelist of each lightcurve file used (files with no transit data are ignored)
+     
     SOMarray_bins = []
     som_ids = []
     SOMarray_binerrors = []
@@ -22,8 +33,9 @@ def PrepareLightcurves(filelist,periods,t0s,tdurs,nbins=50):
         print i
         if os.path.exists(infile):
             #load lightcurve file  REMOVE SKIP HEADER AND FIRST ROW CUT IN PRACTICE
-            lc = np.genfromtxt(infile,skip_header=35)
-            lc = lc[:,(0,2,3)]
+            #lc = np.genfromtxt(infile,skip_header=35)
+            #lc = lc[:,(0,2,3)]
+            lc = np.genfromtxt(infile)
             lc = lc[~np.isnan(lc[:,1]),:]
 
             #get period,T0, Tdur for KOI data
@@ -46,7 +58,7 @@ def PrepareLightcurves(filelist,periods,t0s,tdurs,nbins=50):
 
             #perform binning
             if len(lc[:,0]) != 0: 
-                #bin
+                
                 binphases,SOMtransit_bin,binerrors = utils.GetBinnedVals(phase,lc[:,1],lc[:,2],lc[:,2],nbins,clip_outliers=5)    
 
             #append to SOMarray:
@@ -55,21 +67,30 @@ def PrepareLightcurves(filelist,periods,t0s,tdurs,nbins=50):
                 SOMarray_binerrors.append(binerrors)
     
     #normalise arrays, and interpolate nans where necessary
-    SOMarray_bins = np.array(SOMarray_bins)
-    SOMarray_binerrors = np.array(SOMarray_binerrors)
-    SOMarray_bins,SOMarray_binerrors = utils.PrepareArrays(SOMarray_bins,SOMarray_binerrors)
+    SOMarray = np.array(SOMarray_bins)
+    SOMarray_errors = np.array(SOMarray_binerrors)
+    SOMarray,SOMarray_errors = utils.PrepareArrays(SOMarray,SOMarray_errors)
     
-    return SOMarray_bins, SOMarray_binerrors, np.array(som_ids)
+    return SOMarray, SOMarray_errors, np.array(som_ids)
   
 #missionflag= 0 for kepler, 1 for k2
 def ClassifyPlanet(SOMarray,SOMerrors,n_mc=1000,som=None,groups=None,missionflag=0,case=1,map_all=np.zeros([5,5,5])-1):
 
-    #INSERT INPUT CHECKS HERE, breakout if fail:
-    	#missionflag 0 or 1
-    	#n_mc an integer, at least 1
-    	#som the appropriate object
-    	#groups an array of ints, length to match SOMarray
-    	#case 1 or 2, if case=1 groups array given
+    #    Produces Theta1 or Theta2 statistic to classify transit shapes
+    #
+    #        Args:
+    #            SOMarray: Array of normalised inputs (e.g. binned transits), of shape [n_inputs,n_bins]. n_inputs > 1
+    #            SOMerrors: Errors corresponding to SOMarray values.
+    #            n_mc: Number of Monte Carlo iterations, default 1000. Must be positive int >=1.
+    #            som: Trained som object, like output from CreateSOM(). Optional. If not provided, previously trained SOMs will be used.
+    #            groups: Required if case=1 and user som provided. Array of ints, one per SOMarray row. 0 for planets, 1 for false positives, 2 for candidates.
+    #            missionflag: 0 for Kepler, 1 for K2. Ignored if user som provided.        
+    #            case: 1 or 2. 1 for Theta1 statistic, 2 for Theta2.
+    #            map_all: previously run output of somtools.MapErrors_MC(). Will be run if not provided
+    #
+    #        Returns:
+    #            planet_prob: Array of Theta1 or Theta2 statistic, one entry for each row in SOMarray.
+
 
     #if no SOM, load our SOM (kepler or k2 depending on keplerflag)
     if not som:
@@ -100,7 +121,7 @@ def ClassifyPlanet(SOMarray,SOMerrors,n_mc=1000,som=None,groups=None,missionflag
     #map_all results
     if (map_all<0).all():
         map_all = somtools.MapErrors_MC(som,SOMarray,SOMerrors,n_mc)
-    
+
 
     #classify (depending on case)
     
@@ -117,6 +138,7 @@ def ClassifyPlanet(SOMarray,SOMerrors,n_mc=1000,som=None,groups=None,missionflag
         
         else:  #create new proportions
             prop ,prop_weights= somtools.Proportions(som.K,mapped,groups,2,som.K.shape[0],som.K.shape[1])
+        print map_all
         class_probs = somtools.Classify(map_all,prop,2,prop_weights) 
         planet_prob = class_probs[:,0] / np.sum(class_probs,axis=1)
  
@@ -131,8 +153,8 @@ def ClassifyPlanet(SOMarray,SOMerrors,n_mc=1000,som=None,groups=None,missionflag
             else:
                 SOMarray_PDVM = np.genfromtxt('SOMarray_PDVM_perfect_norm.txt')
                 groups_PDVM = np.genfromtxt('groups_PDVM_perfect_norm.txt')
-                lowbound = np.floor(SOMarray.shape[1]/3).astype('int')
-                testdistances = somtools.PixelClassifier(som.K,SOMarray_PDVM,groups_PDVM,6,lowbound=lowbound,highbound=2*lowbound)
+                lowbound = np.floor(SOMarray.shape[1]/3).astype('int')-1
+                testdistances = somtools.PixelClassifier(som.K,SOMarray_PDVM,groups_PDVM,6,lowbound=lowbound,highbound=2*lowbound+3)
             
             #apply classification
             planet_prob,class_power = somtools.Classify_Distances(map_all,testdistances)
@@ -141,19 +163,34 @@ def ClassifyPlanet(SOMarray,SOMerrors,n_mc=1000,som=None,groups=None,missionflag
     return planet_prob
     
     
-#somshape = (int,int)
 def CreateSOM(SOMarray,niter=500,learningrate=0.1,learningradius=None,somshape=(20,20),outfile=None):
-
+    
+    #    Trains a SOM.
+    #
+    #        Args:
+    #            SOMarray: Array of normalised inputs (e.g. binned transits), of shape [n_inputs,n_bins]. n_inputs > 1
+    #            niter: number of training iterations, default 500. Must be positive integer.
+    #            learningrate: alpha parameter, default 0.1. Must be positive.
+    #            learningradius: sigma parameter, default the largest input SOM dimension. Must be positive.
+    #            somshape: shape of SOM to train, default (20,20). Currently must be 2 dimensional (int, int). Need not be square.
+    #            outfile: File path to save SOM Kohonen Layer to. If None will not save.
+    #
+    #        Returns:
+    #            The trained SOM object
+        
     nbins = SOMarray.shape[1]
     
+    #default learning radius
     if not learningradius:
         learningradius = np.max(somshape)
     
+    #define som initialisation function
     def Init(sample):
         return np.random.uniform(0,2,size=(somshape[0],somshape[1],nbins))
     
     #initialise som
     som = selfsom.SimpleSOMMapper(somshape,niter,initialization_func=Init,learning_rate=learningrate,iradius=learningradius)
+
     #train som
     som.train(SOMarray)
 
--- TransitSOM_Release/utils.py
+++ TransitSOM_Release/utils.py
@@ -26,17 +26,21 @@ def WeightMean_witherror(vals):
     return mean,error
 
 def GetBinnedVals(time,x,lowerr,higherr,bins,clip_outliers=0):
-    #tobin = []
-    #for i in range(len(x)):
-    #    tobin.append([x[i],np.power(2./(higherr[i]+lowerr[i]),2),(higherr[i]+lowerr[i])/2.,clip_outliers])
-    tobin = np.zeros([len(x),4])
+
+    ###
+    #bins x using the mean, weighted by the inverse error squared
+    #if clip_outliers is not zero, it will be used as a significance threshold. 
+    #data more than clip_outliers*its error from the unclipped bin mean will be 
+    #ignored in producing the final bin values
+    ###
+    
+    tobin = np.zeros([len(x),3])
     tobin[:,0] = x
-    tobin[:,1] = np.power(2./(higherr+lowerr),2)  #inverse errors squared
+    tobin[:,1] = np.power(2./(higherr+lowerr),2)
     tobin[:,2] = (higherr+lowerr)/2.
-    tobin[:,3] = clip_outliers
 
     bin_edges = np.linspace(time[0], time[-1], bins+1)
-    bin_edges[-1]+=0.0001
+    bin_edges[-1]+=0.0001                               #avoids edge problems
     binnumber = np.digitize(time, bin_edges)
     timemeans = np.zeros(bins)
     if clip_outliers:
@@ -47,7 +51,6 @@ def GetBinnedVals(time,x,lowerr,higherr,bins,clip_outliers=0):
             else:
                 timemeans[i] = np.nan
     
-        #timemeans, bin_edges, binnumber = stats.binned_statistic(time,tobin,statistic=WeightMeanClipped,bins=bins)
     else:
         for i in range(bins):
             segment = tobin[binnumber == i+1]
@@ -55,7 +58,6 @@ def GetBinnedVals(time,x,lowerr,higherr,bins,clip_outliers=0):
                 timemeans[i] = WeightMean(segment)
             else:
                 timemeans[i] = np.nan
-        #timemeans, bin_edges, binnumber = stats.binned_statistic(time,tobin,statistic=WeightMean,bins=bins)
     binerrors = np.zeros(len(timemeans))
 
     for bin in range(len(timemeans)):
@@ -67,8 +69,7 @@ def GetBinnedVals(time,x,lowerr,higherr,bins,clip_outliers=0):
             binerrors[bin] = np.sqrt(1./np.sum(weights[~outliers])) 
         else:
             binerrors[bin] = np.sqrt(1./np.sum(weights))
-        #binerrors[bin] = np.std(Aps_offset[binnumber==bin+1])/np.sqrt(np.sum(binnumber==bin+1))
-    
+
     binnedtimes = (bin_edges[1:]+bin_edges[:-1])/2.
 
     return binnedtimes,timemeans,binerrors
@@ -98,7 +99,8 @@ def GetBinnedVals_nooutliers(time,x,lowerr,higherr,bins):
 
 def PrepareArrays(SOMarray,errorarray):
       
-    #replace nans, normalise transits, select high SNR objects
+    #replace nans with interpolation, normalise transits
+    
     for idx in range(SOMarray.shape[0]):
         mask = np.isnan(SOMarray[idx,:])
         SOMarray[idx,mask] = np.interp(np.flatnonzero(mask), np.flatnonzero(~mask), SOMarray[idx,~mask])
@@ -108,11 +110,17 @@ def PrepareArrays(SOMarray,errorarray):
     
     
 def SOMNormalise(flux,errors):
+
+    #normalises flux so that mean of lowest quarter of points is 0, and mean of highest quarter of points is 1.
+    #assumes flux consists of 3 transit durations or similar, otherwise normalisation will be wrong.
+    
+    npoints = len(flux)
     sorted = np.sort(flux)
-    lowlevel = np.mean(sorted[:5])
+    lowlevel = np.mean(sorted[:npoints/4-1])
+    norm = 1./(np.mean(sorted[-npoints/4:])-lowlevel)
     flux -= lowlevel
-    flux *= 1./(np.mean(sorted[-30:])-lowlevel)
-    errors = errors * 1./(np.mean(sorted[-30:])-lowlevel)
+    flux *= norm
+    errors *= norm
     return flux,errors
 
 

3c15ccacda3f4d8cc7cee4a5e36562550ffac4a5
David Armstrong <DJArmstrong@users.noreply.github.com>
2016-09-05 15:10:23 +0100

    Added licenses

--- TransitSOM_Release/selfsom.py
+++ TransitSOM_Release/selfsom.py
@@ -1,11 +1,27 @@
-# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
-# vi: set ft=python sts=4 ts=4 sw=4 et:
+# This file is a modified part of the PyMVPA package, distributed as a part of the 
+# TransitSOM code accompanying the paper Armstrong et al 2016.
+#
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
 #
 #   See COPYING file distributed along with the PyMVPA package for the
 #   copyright and license terms.
 #
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 """Self-organizing map (SOM)."""
 
 __docformat__ = 'restructuredtext'
